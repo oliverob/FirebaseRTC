27e5f708864020a577925bbc5e4bfe1623ffcb64
Oliver O'Brien <oliverobrien111@gmail.com>
2020-06-11 17:48:35 +0100

    Update app.js

--- public/app.js
+++ public/app.js
@@ -31,6 +31,7 @@ async function createRoom() {
   document.querySelector('#createBtn').disabled = true;
   document.querySelector('#joinBtn').disabled = true;
   const db = firebase.firestore();
+  const roomRef = await db.collection('rooms').doc();
 
   console.log('Create PeerConnection with configuration: ', configuration);
   peerConnection = new RTCPeerConnection(configuration);
